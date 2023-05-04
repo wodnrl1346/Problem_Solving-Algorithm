adc09637267180a0cc4bc2adc6e671fba2da4b8c
wodnrl1346 <wodnrl1346@gmail.com>
2023-05-04 23:26:19 +0900

    230504

--- 2_Implementation-Brute-force search/백준_19532_수학은 비대면강의입니다.py
+++ 2_Implementation-Brute-force search/백준_19532_수학은 비대면강의입니다.py
@@ -8,3 +8,28 @@ for x in range(-999, 1000):
     if a*x + b*y == c and d * x + e * y == f:
       print(x, y)
       break
+    
+    
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main(void) {
+
+	int a, b, c, d, e, f;
+
+	cin >> a >> b >> c >> d >> e >> f;
+
+	for (int i = -999; i < 1000; i++) {
+		for (int j = -999; j < 1000; j++) {
+			if (a * i + b * j == c) {
+				if (d * i + e * j == f) {
+					cout << i << " " << j;
+					break;
+				}
+			}
+		}
+	}
+}
