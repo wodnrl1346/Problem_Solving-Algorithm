70645b4c61708f46e1adaf85c71db89794a78615
wodnrl1346 <wodnrl1346@gmail.com>
2023-05-04 23:38:41 +0900

    230504

--- 2_Implementation-Brute-force search/백준_19532_수학은 비대면강의입니다.py
+++ 2_Implementation-Brute-force search/백준_19532_수학은 비대면강의입니다.py
@@ -22,8 +22,8 @@ int main(void) {
 
 	cin >> a >> b >> c >> d >> e >> f;
 
-	for (int i = -999; i < 1000; i++) {
-		for (int j = -999; j < 1000; j++) {
+	for (int i = -999; i <= 999; i++) {
+		for (int j = -999; j <= 999; j++) {
 			if (a * i + b * j == c) {
 				if (d * i + e * j == f) {
 					cout << i << " " << j;
