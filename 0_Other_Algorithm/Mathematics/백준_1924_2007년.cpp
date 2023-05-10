e2cd514e912995ef046b19692a7fd39dccf90fdc
wodnrl1346 <wodnrl1346@gmail.com>
2023-05-10 13:39:54 +0900

    230510

--- 0_Other_Algorithm/Mathematics/백준_1924_2007년.cpp
+++ 0_Other_Algorithm/Mathematics/백준_1924_2007년.cpp
@@ -17,3 +17,22 @@ int main(){
   cout << week[y % 7] << '\n';
   return 0;
 }
+
+
+// 내 풀이
+#include <iostream>
+using namespace std;
+
+int main(){
+  int x, y, result=0;
+  cin >> x >> y;
+
+  int month[13] = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+  const char* week[7] = {"SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"};
+
+  for (int i=1; i<x; i++){
+    result += month[i];
+  }
+  result += y-1;
+  cout << week[1 + (result % 7)] << endl;
+}
