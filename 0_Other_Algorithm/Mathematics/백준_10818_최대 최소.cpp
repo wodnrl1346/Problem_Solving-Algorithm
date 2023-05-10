ed623dcd4da3efaa68245c16a21183a7b1473cac
wodnrl1346 <wodnrl1346@gmail.com>
2023-05-10 20:21:34 +0900

    230510

--- 0_Other_Algorithm/Mathematics/백준_10818_최대 최소.cpp
+++ 0_Other_Algorithm/Mathematics/백준_10818_최대 최소.cpp
@@ -1,27 +1,4 @@
-// 1. vector + Indexing
-#include <iostream>
-#include <vector>
-#include <algorithm>
-
-using namespace std;
-
-int main(){
-  int n;
-  cin >> n;
-  vector<int> v;
-
-  int number;
-  for (int i=0; i<n; i++){
-    cin >> number;
-    v.push_back(number);
-  }
-
-  sort(v.begin(), v.end());
-
-  cout << v[0] << " " << v[n-1];
-}
-
-//2. Array + 
+//1. Array
 #include <iostream>
 #include <algorithm>
 #include <vector>
@@ -42,6 +19,30 @@ int main(){
   cout << array[0] << " " << array[n-1];
 
   return 0;
+}
+
+
+// 2. vector + sorting -> indexing
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main(){
+  int n;
+  cin >> n;
+  vector<int> v;
+
+  int number;
+  for (int i=0; i<n; i++){
+    cin >> number;
+    v.push_back(number);
+  }
+
+  sort(v.begin(), v.end());
+
+  cout << v[0] << " " << v[n-1];
 }
 
 //3. Vector + max, min function
@@ -66,7 +67,6 @@ int main(){
   for (int i=0; i<n; i++){
     minvalue = min(minvalue, v[i]);
     maxvalue = max(maxvalue, v[i]);
-
   }
 
   cout << minvalue << " " << maxvalue;
@@ -93,14 +93,10 @@ int main(){
   int maxvalue = -1000001;
 
   for (int i=0; i<n; i++){
-    if(v[i] < minvalue){
-      minvalue = v[i];
-    }
-
-    else if (v[i] > maxvalue){
-      maxvalue = v[i];
-    }
+    if(v[i] < minvalue) minvalue = v[i];
+    if(v[i] > maxvalue) maxvalue = v[i];
   }
+
   cout << minvalue << " " << maxvalue;
 
 }
